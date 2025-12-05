d1db351afca95eb5e8c64e8fbe6ebde3a30017cf
asilya1102 <asilya1993@gmail.com>
2025-12-05 17:57:49 +0500

    Create hw15.py

---
+++ lesson-15/homework/hw15.py
@@ -0,0 +1,19 @@
+CREATE TABLE Roster (
+    Name TEXT,
+    Species TEXT,
+    Age INTEGER
+);
+
+INSERT INTO Roster (Name, Species, Age)
+VALUES
+    ('Benjamin Sisko', 'Human', 40),
+    ('Jadzia Dax', 'Trill', 300),
+    ('Kira Nerys', 'Bajoran', 29);
+
+UPDATE Roster
+SET Name = 'Ezri Dax'
+WHERE Name = 'Jadzia Dax';
+
+SELECT Name, Age
+FROM Roster
+WHERE Species = 'Bajoran';
